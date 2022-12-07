1da5ac6a7f718c92ee08c44174f82a6b275ef08d
VoloshinaElena <119916764+VoloshinaElena@users.noreply.github.com>
2022-12-07 12:49:01 +0000

    Обновлён блок с адресами

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,15 +3,16 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column p-4 gap-2 mt-4 col-4 border-addresses'>
+        <div className='d-flex justify-content-between flex-column p-4 gap-2 mt-4 col-4 border-addresses'>
+        <div className='d-inline-flex flex-column gap-3 px-o py-4'>
             <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3 px-o py-4'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-            <div/>
-            <div className='d-flex justify-content-between flex-column gap-3 px-o py-4'></div>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
+            <div className='d-inline-flex flex-column gap-3 px-o py-4'>
+                <SecondaryButton>Добавить филиал</SecondaryButton>
+                </div>
+            
         </div>
     );
 }
