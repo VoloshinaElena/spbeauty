17cb7d2618ae3163a14838ca652b5c30577ea8ac
VoloshinaElena <119916764+VoloshinaElena@users.noreply.github.com>
2022-12-07 10:55:03 +0000

    Обновлены цвета в инпутах

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -10,7 +10,7 @@ const Select = styled.select`
     border-radius: 24px;
     border: solid #E2E8F0 1px;
     background-color: #F8FAFC;
-    color: #666787;
+    color: #0F172A;
     &:hover {
         background-color: #D9E2EFB8;
     }
