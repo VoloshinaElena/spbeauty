ef85043f4873ffc52f8c5d52f43fe49bd773da5c
VoloshinaElena <119916764+VoloshinaElena@users.noreply.github.com>
2022-12-08 05:47:00 +0000

    Настроены отступы в блоке с адресами

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,13 +3,13 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-flex justify-content-between flex-column p-4 gap-2 mt-4 col-4 border-addresses'>
-        <div className='d-inline-flex flex-column gap-3 px-o py-4'>
+        <div className='d-flex justify-content-between flex-column p-4 col-4 border-addresses'>
+        <div className='d-inline-flex flex-column gap-4'>
             <div className='title'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
             </div>
-            <div className='d-inline-flex flex-column gap-3 px-o py-4'>
+            <div className='d-inline-flex flex-column'>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
                 </div>
             
